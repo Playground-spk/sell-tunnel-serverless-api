704c0ccde88fac617b7028635fe21565a2fba7a3
supakon.c <taresupakon@gmail.com>
2022-05-05 19:22:11 +0700

    init project

--- lib/sell-tunnel-serverless-stack.ts
+++ lib/sell-tunnel-serverless-stack.ts
@@ -1,4 +1,4 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Stack, StackProps, aws_s3 as s3 } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -12,5 +12,9 @@ export class SellTunnelServerlessStack extends Stack {
     // const queue = new sqs.Queue(this, 'SellTunnelServerlessQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+
+    new s3.Bucket(this, 'MyFirstBucket', {
+      versioned: true
+    });
   }
 }
