1782e664d61a7867c4d3c128737b94a6ce98d53e
supakon.c <taresupakon@gmail.com>
2022-05-28 22:21:43 +0700

    Initial commit

---
+++ test/sell-tunnel-serverless.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SellTunnelServerless from '../lib/sell-tunnel-serverless-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/sell-tunnel-serverless-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SellTunnelServerless.SellTunnelServerlessStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
